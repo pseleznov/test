e19ee30c9664bc3279a79a4ec73086e2c6f3e8aa
pseleznov <pashaselesniov1@gmail.com>
2020-03-20 16:19:05 +0200

    new search + adaptive header

--- src/components/Header/HeaderMenu/HeaderMenu.jsx
+++ src/components/Header/HeaderMenu/HeaderMenu.jsx
@@ -3,46 +3,64 @@ import './HeaderMenu.css'
 import { NavLink } from 'react-router-dom'
 import vector from '../../../assets/images/Vector.svg'
 
+const state = [
+    'Главная',
+    'Меню'
+];
+
+const BurgerOn = {
+    on: false
+}
+
+
 const HeaderMenu = () => {
     return (
-        <div className="headerMenu">
-            <div className="headerMenuItem">
-                <div>
-                    <NavLink to='/homepage' activeClassName='active'>Главная</NavLink>
-                    <div className='headerMenuItem_underline'></div>
-                </div>
-            </div>
-            <div className="headerMenuLine"></div>
-            <div className="headerMenuItem">
-                <div className='headerMenuItemServ'>
+        <div className='burgerMenu'>
+            <input type='checkbox' id='a1' className='checkbox' />
+            <label htmlFor='a1' className='burgerLabel'>
+                <span>
+                    <div></div>
+                </span>
+            </label>   
+            <div className="headerMenu">
+                <div className="headerMenuItem">
                     <div>
-                        <NavLink to='/services' activeClassName='active'>Услуги</NavLink>
+                        <NavLink to='/homepage' activeClassName='active'>Главная</NavLink>
                         <div className='headerMenuItem_underline'></div>
                     </div>
-                    <div className='vector-img'>
-                        <img src={vector} alt="" />
+                </div>
+                <div className="headerMenuLine"></div>
+                <div className="headerMenuItem">
+                    <div className='headerMenuItemServ'>
+                        <div>
+                            <NavLink to='/services' activeClassName='active'>Услуги</NavLink>
+                            <div className='headerMenuItem_underline'></div>
+                        </div>
+                        <div className='vectorImg'>
+                            <img src={vector} alt="" />
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="headerMenuLine"></div>
-            <div className="headerMenuItem">
-                <div>
-                    <NavLink to='/ourwork' activeClassName='active'>Наши работы</NavLink>
-                    <div className='headerMenuItem_underline'></div>
+                <div className="headerMenuLine"></div>
+                <div className="headerMenuItem">
+                    <div>
+                        <NavLink to='/ourwork' activeClassName='active'>Наши работы</NavLink>
+                        <div className='headerMenuItem_underline'></div>
+                    </div>
                 </div>
-            </div>
-            <div className="headerMenuLine"></div>
-            <div className="headerMenuItem">
-                <div>
-                    <NavLink to='aboutus' activeClassName='active'>О нас</NavLink>
-                    <div className='headerMenuItem_underline'></div>
+                <div className="headerMenuLine"></div>
+                <div className="headerMenuItem">
+                    <div>
+                        <NavLink to='aboutus' activeClassName='active'>О нас</NavLink>
+                        <div className='headerMenuItem_underline'></div>
+                    </div>
                 </div>
-            </div>
-            <div className="headerMenuLine"></div>
-            <div className="headerMenuItem">
-                <div>
-                    <NavLink to='contacts' activeClassName='active'>Контакты</NavLink>
-                    <div className='headerMenuItem_underline'></div>
+                <div className="headerMenuLine"></div>
+                <div className="headerMenuItem">
+                    <div>
+                        <NavLink to='contacts' activeClassName='active'>Контакты</NavLink>
+                        <div className='headerMenuItem_underline'></div>
+                    </div>
                 </div>
             </div>
         </div>
