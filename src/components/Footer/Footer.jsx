45b92ae491dc87fe081d69b87f31dda8052f11cd
pseleznov <pashaselesniov1@gmail.com>
2020-03-22 04:15:49 +0200

    adding footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
-import  './Footer.css'
+import './Footer.css'
 
 const Footer = () => {
     return (
-        <div className="bodyWrapper">
-            FOOTER
+        <div className="footerWrapper">
+            <div className="footergrid">
+                <div className="info">
+                    <div className="infoWrapper">
+                        <span>О компании</span>
+                        <span>Наши работы</span>
+                        <span>Контакты</span>
+                        <span>Доставка</span>
+                        <span>Форма заказа</span>
+                    </div>
+                </div>
+                <div className="service">
+                    <div className="serviceWrapper">
+                        <div>Услуги</div>
+                        <div className="serviceBlock">
+                            <div className="firstServiceBlock">
+                                <div>Lorem ipsum dolor sit amet.</div>
+                                <div>Lorem ipsum dolor sit amet.</div>
+                                <div>Lorem ipsum dolor sit amet.</div>
+                                <div>Lorem ipsum dolor sit amet.</div>
+                            </div>
+                            <div className="secondServiceBlock">
+                                <div>Lorem ipsum dolor sit amet.</div>
+                                <div>Lorem ipsum dolor sit amet.</div>
+                                <div>Lorem ipsum dolor sit amet.</div>
+                                <div>Lorem ipsum dolor sit amet.</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="contacts">asdas</div>
+            </div>
+            <div className="directed_By_Robert_B_Weide"></div>
         </div>
     );
 }
